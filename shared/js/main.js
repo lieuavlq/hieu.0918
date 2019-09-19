f7be8357c93f469d124f2a6e561cf1f1a4f435fc
lieuav <lieuav@gmail.com>
2019-09-20 00:10:41 +0700

    update page

--- shared/js/main.js
+++ shared/js/main.js
@@ -1,3 +1,23 @@
 $(function() {
-  // alert('aaaa')
+  $('.primary-menu > li.active > a').hover(function() {
+    $('.sub-menu').addClass('active');
+  });
+  $('.primary-menu > li:not(.active) > a').hover(function() {
+    $('.sub-menu').removeClass('active');
+  });
+  $('.sub-menu').hover(function() {
+    $(".sub-menu").addClass('subactive');
+  }, function() {
+    $(".sub-menu").removeClass("active");
+  });
+  $('.slider').slick({
+    infinite: true,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
 });
